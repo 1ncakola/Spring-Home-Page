2755bf1b3a34114d99147c239c52539eb3fc306c
1ncakola <150153027+1ncakola@users.noreply.github.com>
2024-02-22 11:52:17 -0330

    Add files via upload

---
+++ sprint.js
@@ -0,0 +1,27 @@
+
+// Function to initialize the website
+function init() {
+    populateMenu();
+    const orderForm = document.getElementById("order-form");
+    orderForm.addEventListener("submit", submitOrder);
+}
+
+// Initialize the website when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", init);
+
+
+document.getElementById("paragraph").style.lineHeight = "1"; 
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var home = document.getElementById("home");
+    var detailBox = document.querySelector(".detailbox");
+    
+    
+    setTimeout(function() {
+      detailBox.style.opacity = "2";
+    }, 100); d
+      
+  });
+
